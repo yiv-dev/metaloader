bcf3ae03f576fecc19cf076b0e27ddd78fc223b3
YIV <yugaev@gmail.com>
2020-12-29 17:57:42 +0300

    v 0.0.2

--- test.php
+++ test.php
@@ -3,7 +3,7 @@ use YIVDEV\METALOADER\Handler;
 require 'src/Handler.php';
 
 $testHandler = new Handler('https://nr8.newradio.it:19574/stream');
-$content = $testHandler->read_stream( '' );
+$content = $testHandler->read_stream();
 print_r("----------------\n");
 print_r($content);
 
